639b6fdfaa66e9c6836057c346beb10b690913e5
Evan Diaz <evand1998@gmail.com>
2022-05-26 16:27:00 -0500

    first commit

---
+++ src/components/Hero/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+function Hero() {
+
+    return (
+        <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="hero-container" data-aos="fade-in">
+                <h1>Evan Diaz</h1>
+                <p>Full Stack Web Developer</p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+   import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'boxicons/css/boxicons.min.css'
+import 'aos/dist/aos.css'
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'aos/dist/aos.js'
+
+ReactDOM.render(
+
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
